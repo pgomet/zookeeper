9666a7af02cc72c988ce513f8aa94b9eda9ae0a7
Pina Gomet <pina@Pinas-MBP.lan>
2022-06-10 16:41:07 -0400

    created a fetch for animals.js

--- server.js
+++ server.js
@@ -65,6 +65,8 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+app.use(express.static('public'));
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -78,6 +80,22 @@ app.post('/api/animals', (req, res) => {
   const animal = createNewAnimal(req.body, animals);
 
   res.json(animal);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
