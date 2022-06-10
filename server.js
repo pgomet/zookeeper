f9c2ffb35c885f1fd7c0a00528c8cdc8d6230d85
Pina Gomet <pina@Pinas-MBP.lan>
2022-06-10 17:27:30 -0400

    created front-end functionality for zookeepers

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { animals } = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -71,6 +73,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
